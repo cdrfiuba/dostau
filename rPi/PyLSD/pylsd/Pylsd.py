dfac923058bf2eedfeaf65384f9d0594c84ffa28
Lucas Chiesa <lucas.chiesa@gmail.com>
2013-06-02 02:20:07 -0300

    Completo el modulo de Python que wrappea LSD. Por ahora el unico metodo que tiene abre un archivo de imagen. Hay que ver de que forma necesitamos pasar la imagen y crear el metodo correspondiente. Hay un test manual que compara la salida del codigo en Python con la salida del programa original usando la foto 'chairs.png'. Es importante notar que como PIL no sabe abrir pgms, ahora funciona con fotos normales (png, jpg, etc) pero no pgms.

--- rPi/PyLSD/pylsd/Pylsd.py
+++ rPi/PyLSD/pylsd/Pylsd.py
@@ -9,12 +9,17 @@ class PyLSD:
 
     @staticmethod
     def lsd(image_file):
-        """ Searchs for all the segments in an image. """
+        """
+        Searchs for all the segments in an image.
 
+        :param image_file: The name of the image file. PIL should be able to open it.
+        :returns: A matrix of N segments by 7.
+        """
         im = Image.open(image_file)
         X, Y = im.size
         image = numpy.array(im.getdata(), dtype=numpy.float64)
         # Calls the C method with all the suggested parameter values.
         s = _pylsd.line_segment_detection(image, X, Y, 0.8, 0.6, 2.0, 22.5, 0.0, 0.7, 1024)
-        return s
-
+        ans = numpy.array(s)
+        ans = ans.reshape((len(s)/7, 7))
+        return ans
--- rPi/PyLSD/tests/test.py
+++ rPi/PyLSD/tests/test.py
@@ -1,5 +1,12 @@
 import pylsd.Pylsd
+from numpy import around
 
 lsd = pylsd.Pylsd.PyLSD()
 
-print lsd.lsd('chairs.png')
+segs = lsd.lsd('chairs.png')
+
+salida = open('python-generated.txt', 'w')
+for seg in segs:
+    seg = around(seg, decimals=6)
+    line = ' '.join("%.6f" % i for i in seg)
+    salida.write(line + ' \n')
